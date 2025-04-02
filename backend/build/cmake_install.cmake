95d9766b2806ff4ac440e0c9c0f8c01fdbc6ffc9
LipaJunior <kamil.lipinski2004@gmail.com>
2025-04-02 15:10:08 +0200

    fixed issused with windows I hope

--- backend/build/cmake_install.cmake
+++ backend/build/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: /Users/georg/Desktop/Project 2-2/applied-cryptography-group08/backend
+# Install script for directory: C:/Users/kamil/Desktop/applied-cryptography-group08/backend
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/usr/local")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/CrowBackend")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -34,13 +34,13 @@ endif()
 
 # Set path to fallback-tool for dependency-resolution.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/Library/Developer/CommandLineTools/usr/bin/objdump")
+  set(CMAKE_OBJDUMP "C:/winlibs/mingw64/bin/objdump.exe")
 endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "/Users/georg/Desktop/Project 2-2/applied-cryptography-group08/backend/build/install_local_manifest.txt"
+  file(WRITE "C:/Users/kamil/Desktop/applied-cryptography-group08/backend/build/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -56,6 +56,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "/Users/georg/Desktop/Project 2-2/applied-cryptography-group08/backend/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/kamil/Desktop/applied-cryptography-group08/backend/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()

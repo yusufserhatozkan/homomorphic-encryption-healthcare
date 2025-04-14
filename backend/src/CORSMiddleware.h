a46c87cbc6cfea52be52ace25657cd8f1cd2a9de
Luca Nichifor <nichiforluca@gmail.com>
2025-04-14 18:19:41 +0200

    Added CSV interface

---
+++ backend/src/CORSMiddleware.h
@@ -0,0 +1,8 @@
+//
+// Created by Luca Nichifor on 4/14/25.
+//
+
+#ifndef CORSMIDDLEWARE_H
+#define CORSMIDDLEWARE_H
+
+#endif //CORSMIDDLEWARE_H
---
+++ backend/src/interfaceCSV.cpp
@@ -0,0 +1,3 @@
+//
+// Created by Luca Nichifor on 4/14/25.
+//
